35f4bba02400b074bea4c5a6d69e5512f921196f
Dakson Cruz <daksonc@gmail.com>
2024-02-19 08:26:29 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Projeto Integrador III API')
+    .setDescription('Api de backend do Projeto Integrador III.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+  await app.listen(3333);
+}
+bootstrap();
